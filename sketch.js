601d81dd030e5f0c5d131e8c48f1fc333eaafbcc
VardhiniAT <71555563+VardhiniAT@users.noreply.github.com>
2020-09-26 13:07:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,54 @@
-var ball,img,paddle;
+var ball,img,paddle,img2;
 function preload() {
-  /* preload your images here of the ball and the paddle */
+  img=loadImage("ball.png");
+  img2=loadImage("paddle.png");
 }
 function setup() {
   createCanvas(400, 400);
-   /* create the Ball Sprite and the Paddle Sprite */
-  /* assign the images to the sprites */
+  ball=createSprite(25,200);
+  ball.addImage("ball",img);
+    ball.velocityY=8;
+  ball.velocityX=-7;
   
-  /* give the ball an initial velocity of 9 in the X direction */
-  
-
+  paddle=createSprite(375,200); 
+  paddle.addImage("paddle",img2);
 }
 
 function draw() {
   background(205,153,0);
-  /* create Edge Sprites here */
+  edges=createEdgeSprites();
   
-  /* Allow the ball sprite to bounceOff the left, top and bottom edges only, leaving the right edge of the canvas to be open. */
+  ball.bounceOff(edges[0]);
+  //ball.bounceOff(edges[1]);
+  ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[3]);
   
-
+  
   /* Allow the ball to bounceoff from the paddle */
   /* Also assign a collision callback function, so that the ball can have a random y velocity, making the game interesting */
+  ball.bounceOff(paddle);
  
   /* Prevent the paddle from going out of the edges */ 
+  paddle.bounce(edges[2]);
+  paddle.bounce(edges[3]);
   
-  
-  if(keyDown(UP_ARROW))
+  if(ball.isTouching(paddle))
   {
-     /* what should happen when you press the UP Arrow Key */
+  ball.velocityY=random(-8,8);
+  ball.velocityX= random(-7,7);
   }
   
-  if(keyDown(DOWN_ARROW))
+  if(keyDown("UP_ARROW"))
   {
-    /* what should happen when you press the UP Arrow Key */
+   paddle.velocityY=-6;
+  }
+  
+  if(keyDown("DOWN_ARROW"))
+  {
+ paddle.velocityY=6;
   }
   drawSprites();
   
 }
 
-function randomVelocity()
-{
-  /* this function gets called when the ball bounces off the paddle */
-  /* assign the ball a random vertical velocity, so it bounces off in random direction */
-}
 
